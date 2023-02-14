e652c656208f44382bec467534521da5c1479bfe
司 文 <1046244213@qq.com>
2023-02-14 11:14:58 +0800

    注释代码

--- src/Core/Domain/System/RoleMenu.cs
+++ src/Core/Domain/System/RoleMenu.cs
@@ -10,6 +10,4 @@ public class RoleMenu : BaseEntity
 {
     public string RoleId { get; set; } = default!;
     public Guid MenuId { get; set; }
-    [NotMapped]
-    public List<DomainEvent> DomainEvents { get; } = new();
 }
